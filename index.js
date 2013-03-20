7c3e18005faf920d5082322b995d3340276fdfee
mafintosh <mathiasbuus@gmail.com>
2013-03-20 20:10:25 +0100

    better algo in the beginning

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 module.exports = function(seconds) {
 	seconds = seconds || 5;
-	var transfers = Array(seconds);
-	for (var i = 0; i < transfers.length; i++) {
-		transfers[i] = {time:Date.now(), transferred:0};
-	}
-
+	var transfers = [{time:Date.now(), transferred:0},{time:Date.now(), transferred:0}];
+	var tail = transfers[1];
 	var head = transfers[0];
-	var tail = transfers[transfers.length-1];
+
 	return function(delta) {
 		delta = delta || 0;
 		head.time = Date.now();
 		head.transferred += delta;
 
-		if (head.time - tail.time > seconds * 1000) {
+		if (head.time - tail.time > transfers.length * 1000) {
 			transfers.unshift({time:head.time, transferred:head.transferred});
-			transfers.pop();
+			if (transfers.length > seconds) transfers.pop();
 			head = transfers[0];
 			tail = transfers[transfers.length-1];
 		}
 
-		return Math.round(1000 * (head.transferred - tail.transferred) / (head.time - tail.time));
+		return 1000 * (head.transferred - tail.transferred) / Math.max(1, head.time - tail.time);
 	};
 };
